2581507bf4b8baae14d64464cb001f5db7c1ae7b
x446688 <xzp2byf924@privaterelay.appleid.com>
2026-05-04 03:06:37 +0300

    Add tests and basic EDA

--- project/src/project/test.py
+++ project/src/project/test.py
@@ -2,5 +2,4 @@ from .datagen import *
 import os
 from pathlib import Path
 def run_all_tests():
-    # print()
     display_series(gen_base_series(), Path(os.path.dirname(__file__) + "/../../data"))
